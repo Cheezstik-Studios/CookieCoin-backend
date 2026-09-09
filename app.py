6a979442a77e81c7f9d5d87c32c2d71841ba9ddb
Cheezstik-Studios <cheezstik.studios@gmail.com>
2026-09-09 16:20:11 +0930

    Enhance Block structure with nonce and target
    Updated Block class to include nonce and target parameters, modified related methods, and adjusted blockchain initialization and validation functions.

--- app.py
+++ app.py
@@ -10,84 +10,84 @@ sock = Sock(app)
 
 #defines the Block class
 class Block: 
-  def __init__(self, index, hash, prevHash, timestamp, data): 
-    # a block has an index, the pervious block's hash, a timestamp, transaction data, and a hash calculated from the rest
+  def __init__(self, index, hash, prevHash, timestamp, data, nonce, target): 
+    # a block has an index, the pervious block's hash, a timestamp, transaction data, a nonce (number used once), a target, and a hash calculated from the rest
     self.index = index
     self.previousHash = prevHash
     self.timestamp = timestamp
     self.data = data
-    self.hash = hash
-
+    self.nonce = nonce
+    self.target = target
+    self.hash = self.calculateHash()
+  
+  def calculateHash(self):
+    # uses the sha256 cryptographic hashing algorithm
+    return hashlib.sha256(
+      (str(self.index) + self.prevHash + str(self.timestamp) + self.data + str(self.nonce)).encode()
+    ).hexdigest()
 # defines how json.dumps() jsonifies Block objects
 class jsonencoder(json.JSONEncoder):
   def default(self, obj):
     if isinstance(obj, Block):
-      return {'index': obj.index, 'hash': obj.hash, 'prevHash': obj.prevHash, 'timestamp': obj.timestamp, 'data': obj.data}
+      return {'index': obj.index, 'hash': obj.hash, 'prevHash': obj.prevHash, 'timestamp': obj.timestamp, 'data': obj.data, 'nonce': obj.nonce, 'target': obj.target}
     
     # just in case I later use this on a non-Block
     return super().default(obj)
-          
-def calculateHash(index, prevHash, timestamp, data):
-  # uses the sha256 cryptographic hashing algorithm
-  return hashlib.sha256(
-    (str(index) + prevHash + str(timestamp) + data).encode()
-  ).hexdigest()
 
 # returns the genesis block
 def genesis():
-  # don't as why that timestamp is being used specifically...
-  timestamp = 1000212390
-  return Block(0, 
-               calculateHash(0, 
-                             '0000000000000000000000000000000000000000000000000000000000000000', # just a fully zero hash
-                             timestamp, 
-                             'You feel like making cookies. But nobody wants to eat your cookies.'), # no transaction data, so i might as well
-               '0000000000000000000000000000000000000000000000000000000000000000', 
-               timestamp, 
-               'You feel like making cookies. But nobody wants to eat your cookies.')
+  return Block(0, # zero-indexed
+    '0000000000000000000000000000000000000000000000000000000000000000', # just a fully zero pervious hash
+    0, # this was made on 1/1/1970 trust me
+    'You feel like making cookies. But nobody wants to eat your cookies.', # no transaction data, so I might as well
+    67, # SIX SE- sorry
+    (2 ** 256 - 1)) 
 
 # initialise the blockchain and peers list
-blocks = [genesis()]
+blockchain = [genesis()]
 connected_peers = []
 
 # random utility functions grouped so they get replaced when I change the blockchain storage method
 def latestBlock():
-  return blocks[-1]
+  return blockchain[-1]
 def getBlockchain():
-  return blocks
-def actuallyReplaceChain(newBlocks):
-  blocks[:] = newBlocks
+  return blockchain
+def actuallyReplaceChain(newBlockchain):
+  blocksblockchain[:] = newBlocks
 
-# not yet
-def broadcastLatest():
-  pass
+def sendLatest():
+  for ws, address in connected_peers:
+    ws.send(json.dumps(getBlockchain(), cls=jsonencoder))
 
 # self-explanatory
-def nextBlock(data):
+def nextBlock(data, nonce, target):
   prevBlock = latestBlock()
   index = prevBlock.index + 1
   prevHash = prevBlock.hash
   timestamp = int(time.time())
-  hash = calculateHash(index, prevHash, timestamp, data)
-  return Block(index, hash, prevHash, timestamp, data)
+  return Block(index, hash, prevHash, timestamp, data, nonce, target)
 
 # validator functions
 def isValidStructure(block):
   return (type(block.index) is int 
-          and type(block.prevHash) is str 
-          and type(block.hash) is str 
-          and type(block.timestamp) is int 
-          and type(block.data) is str)
+    and type(block.prevHash) is str 
+    and type(block.hash) is str 
+    and type(block.timestamp) is int 
+    and type(block.data) is str
+    and type(block.nonce) is number
+    and type(block.target) is number)
   
 def isValidBlock(block, prevBlock):
+  if not isValidStructure(block):
+    return False
   if prevBlock.index + 1 != block.index:
     return False
   elif prevBlock.hash != block.prevHash:
     return False
-  elif calculateHash(block.index, block.prevHash, block.timestamp, block.data) != block.hash:
+  if not hashIsDifficulty(block.hash, block.target):
     return False
   
-  return True and isValidStructure(block)
+  return True
 
 def isValidChain(blockchain):
   if blockchain[0] != genesis():
@@ -99,11 +99,16 @@ def isValidChain(blockchain):
 
   return True
 
+# checks if a block has a valid difficulty
+def hashIsDifficulty(hash, target):
+  number = int(hash, 16)
+  return number <= target
+
 # replaces the chain if it's valid and broadcasts it
-def replaceChain(newBlocks):
+def replaceChain(ws, newBlocks):
   if isValidChain(newBlocks) and len(newBlocks) > len(getBlockchain()):
     actuallyReplaceChain(newBlocks)
-    broadcastLatest()
+    sendLatest()
 
 @app.route('/blocks')
 def blocks():
@@ -112,7 +117,7 @@ def blocks():
 @sock.route('/mineblock')
 def mineblock(ws):
   newBlock = nextBlock(ws.receive())
-  ws.send(json.dumps(newBlock))
+  ws.send(json.dumps(newBlock, cls=jsonencoder))
 
 @sock.route('/connect')
 def connect(ws):
